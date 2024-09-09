48d1c778866c56cfd63608f84d2ed4ed60202400
kauank8 <kauan_live360@hotmail.com>
2024-09-09 11:09:31 -0300

    Personalizando Nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'; // Para os ícones
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [ MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
