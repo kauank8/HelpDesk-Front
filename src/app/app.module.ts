c0970bed3d37f87c2bd46adade5e2ef5bc26f6f9
kauank8 <kauan_live360@hotmail.com>
2024-09-09 09:14:49 -0300

    Criando rota e componente NAV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,9 +25,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-
+import { NavComponent } from './components/nav/nav.component';
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent
+    ,NavComponent],
   imports: [
     CommonModule,
     // Forms
